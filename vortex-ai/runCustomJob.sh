1d0c4174b18a16e5a0d3cc74ce9c800961f09967
Francisco Fuentes Barrera <ffuentes@emergya.com>
2024-02-26 11:01:32 +0100

    Example training files

--- vortex-ai/runCustomJob.sh
+++ vortex-ai/runCustomJob.sh
@@ -2,4 +2,5 @@ gcloud ai custom-jobs create \
   --project=test-cf-411521 \
   --region=europe-west1 \
   --display-name="ArgosTrain for Spanish to Extremaduran" \
-  --config=ext_es_config.yaml
+  --config=ext_es_config.yaml \
+  --command="./entrypoint_es_ext.sh"
